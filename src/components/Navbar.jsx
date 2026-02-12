2c6142979ffed3573aa0572bd850b74849ed3a55
llcfernandes <lucasmividi@gmail.com>
2026-02-12 20:29:55 -0300

    Start interface implementation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Navbar = ({theme, setTheme}) => {
+  return (
+    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+      
+    <img src={theme === 'dark' ? assets.logo_dark : assets.logo}className='w-32 sm:w-40' alt="logo" />
+
+    <div className='text-gray-700 ddark:text-white sm: text-sm'>
+        <a href="#" className='sm:hover:border-b'>Home</a>
+        <a href="#services" className='sm:hover:border-b'>Services</a>
+        <a href="#our-work" className='sm:hover:border-b'>Our Work</a>
+        <a href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
+    </div>
+
+    </div>
+  )
+}
+
+export default Navbar
