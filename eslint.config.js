126cc19b50ac8a9ba1418f8ce0ccfd96fba35216
llcfernandes <lucasmividi@gmail.com>
2026-02-28 16:47:41 -0300

    Complete website by adding footer and implementing global animations with Motion

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,16 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -23,7 +27,8 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'off',
+      'react/jsx-uses-vars': 'error',
     },
   },
 ])
